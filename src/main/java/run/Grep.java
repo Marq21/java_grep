505ead6811ebb0593049f4a2670615c745b96318
Ковыршин Николай <nikolaykovyrshin@outlook.com>
2022-10-28 00:32:00 +0300

    block try/catch in 'Grep'

--- src/main/java/run/Grep.java
+++ src/main/java/run/Grep.java
@@ -19,7 +19,7 @@ public class Grep {
             + "search options:\n "
             + "\t- '-i' - for regExp search;\n"
             + "\t- '-w' - for ignoreCase search;\n"
-            + "tap 'grep stop' - for stopping app;"
+            + "tap 'grep stop' - for stopping app;\n"
             + "tap 'grep help' - for show this message again";
 
     public void runGrep() {
@@ -31,10 +31,14 @@ public class Grep {
 
         if (inputCommand.equals(command.getCommand())) {
             while (!command.equals(Command.JAVA_GREP_STOP)) {
-                list = List.of(sc.next().split("\\s"));
-                String key = list.get(list.size()-1);
-                grepSeeker = GrepSeekerInitializer.initialize(list);
-                grepSeeker.find(key);
+                try {
+                    list = List.of(sc.next().split("\\s"));
+                    String key = list.get(list.size()-1);
+                    grepSeeker = GrepSeekerInitializer.initialize(list);
+                    grepSeeker.find(key);
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("incorrect command, try to input 'grep help' to see all commands");
+                }
             }
         } else {
             System.out.println("incorrect command, try to input 'grep help' to see all commands");
